99868a4ffef0f59aa395a8def0922d5fb6b7b9b9
Daniel Schermuk <daniel.schermuk@gmail.com>
2016-04-15 09:15:02 -0300

    Angular Material Sidenav and Toolbar

--- public/javascripts/controllers/master-ctrl.js
+++ public/javascripts/controllers/master-ctrl.js
@@ -3,9 +3,9 @@
  */
 
 angular.module('RDash')
-    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+    .controller('MasterCtrl', ['$scope', '$cookieStore','$mdSidenav', '$timeout', MasterCtrl]);
 
-function MasterCtrl($scope, $cookieStore) {
+function MasterCtrl($scope, $cookieStore, $mdSidenav, $timeout) {
     /**
      * Sidebar Toggle & Cookie Control
      */
@@ -36,4 +36,80 @@ function MasterCtrl($scope, $cookieStore) {
     window.onresize = function() {
         $scope.$apply();
     };
+	
+	
+	/**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    $scope.debounce = function(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    };
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    $scope.buildDelayedToggler=function (navID) {
+      return $scope.debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID).toggle();
+      }, 200);
+    };
+    
+	$scope.buildToggler = function (navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    };
+	
+	$scope.toggleLeft = $scope.buildDelayedToggler('left');	
+	
+	$scope.menues = [	{	"name": 	"Dashboard",
+							"url":	"#",
+							"type":	"item"
+						}, 
+						{
+							"name": 	"Rooms",
+							"url":	"#",
+							"type":	"menu"
+						} ];
+	$scope.menues[1].items =  [{	"name": "Room1",
+											"url":	"#",
+											"type":	"item"
+									   }, {	
+											"name": "Room2",
+											"url":	"#",
+											"type":	"item"
+									   },{	
+											"name": "Room3",
+											"url":	"#",
+											"type":	"item"
+									   } ];
+	
 }
+
+angular.module('RDash').controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+  })
+  
+    

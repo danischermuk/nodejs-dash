79bf09241256b355a5dff0a146f630fdc5474f40
danischermuk <daniel.schermuk@gmail.com>
2016-12-02 12:54:17 -0300

    Arreglos en el Sketch de Arduino

--- Arduino/WifiSwitch/WifiSwitch.ino
+++ Arduino/WifiSwitch/WifiSwitch.ino
@@ -33,10 +33,6 @@ unsigned int localUdpPort = 6789;
 long lastHB = 0;
 IPAddress ip;
 char myIpString[24];
-#ifdef MEDIR_TIEMPOS
-long tiempo=0;
-#endif
-
 
 int charArrayLength( char* array, int max) {
   int i=0;
@@ -50,33 +46,14 @@ int charArrayLength( char* array, int max) {
 
 
 void setup() {
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   Serial.begin(115200);
   delay(10);
-#ifdef MEDIR_TIEMPOS
-  Serial.print("Serial.Begin: ");
-  Serial.println(millis()-tiempo);
-#endif
 
-
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   EEPROM.begin(512);
-#ifdef MEDIR_TIEMPOS
-  Serial.print("EEPROM.Begin: ");
-  Serial.println(millis()-tiempo);
-#endif  
   // prepare GPIO2
   pinMode(2, OUTPUT);
   digitalWrite(2, 0);
   
-  // Verificando la configuración de EEPROM
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   EEPROM_readAnything(GENERALCONFIG_EEPROM_ADDR, GeneralConfig);
   int GeneralConfigCrc = crc((byte*)&GeneralConfig, sizeof(GeneralConfig) - sizeof(GeneralConfig.crc));
   if (GeneralConfigCrc == GeneralConfig.crc){
@@ -87,10 +64,6 @@ void setup() {
     Serial.println("Error al leer la configuracion. Cargando defaults...");
     GeneralConfig_LoadDefaults();
   }
-#ifdef MEDIR_TIEMPOS
-  Serial.print("GeneralConfig Load: ");
-  Serial.println(millis()-tiempo);
-#endif
 #ifdef DEBUG
   // Para hacer modificaciones en el GeneralConfig a la fuerza
   GeneralConfig_LoadDefaults();
@@ -103,48 +76,22 @@ void setup() {
   Serial.println(GeneralConfig.WifiConfig.ssid);
 
   // Verifico la configuracion de la IP
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   if (GeneralConfig.WifiConfig.dhcp == false)
   {
    WiFi.config(GeneralConfig.WifiConfig.ip, GeneralConfig.WifiConfig.gateway, GeneralConfig.WifiConfig.subnet); 
   }
-#ifdef MEDIR_TIEMPOS
-  Serial.print("WiFi.Config: ");
-  Serial.println(millis()-tiempo);
-#endif
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
+
+
   WiFi.begin(GeneralConfig.WifiConfig.ssid, GeneralConfig.WifiConfig.pass);
-#ifdef MEDIR_TIEMPOS
-  Serial.print("WiFi.Begin: ");
-  Serial.println(millis()-tiempo);
-#endif
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
     Serial.print(".");
   }
-#ifdef MEDIR_TIEMPOS
-  Serial.print("WiFi.Status: ");
-  Serial.println(millis()-tiempo);
-#endif
   Serial.println("");
   Serial.println("WiFi connected");
 
   // Start the server
-#ifdef MEDIR_TIEMPOS
-  tiempo=millis();
-#endif
   server.begin();
-#ifdef MEDIR_TIEMPOS
-  Serial.print("Server.Begin: ");
-  Serial.println(millis()-tiempo);
-#endif
   Serial.println("Server started");
   // Print the IP address
   Serial.println(WiFi.localIP());
@@ -179,12 +126,14 @@ void loop() {
   conf.printTo(Serial);
   Serial.println();
   conf.printTo(message, sizeof(message));
-
+  
   
   // transmit broadcast package
   Udp.beginPacket(ip, port);
   Udp.write(message);
   Udp.endPacket();
+
+  
   delay(5000);
 }
 
